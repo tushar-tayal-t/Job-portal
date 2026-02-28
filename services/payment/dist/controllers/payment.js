bd6b3f01088c67eb459ae3d174cc105101c73723
tushar-tayal-t <tushartayal315@gmail.com>
2026-02-28 23:26:21 +0530

    Working on payment gateway integration stripe

---
+++ services/payment/dist/controllers/payment.js
@@ -0,0 +1,52 @@
+import sql from "../utils/db.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import { TryCatch } from "../utils/TryCatch.js";
+import { stripe } from "../index.js";
+export const checkOut = TryCatch(async (req, res) => {
+    if (!req.user) {
+        new ErrorHandler(401, "No valid User");
+    }
+    const userId = req.user?.user_id;
+    const [user] = await sql `
+    SELECT * FROM users WHERE user_id=${userId}
+  `;
+    const subTime = user?.subscription
+        ? new Date(user.subscription).getTime()
+        : 0;
+    const now = Date.now();
+    const isSubcribed = subTime > now;
+    if (isSubcribed) {
+        throw new ErrorHandler(400, "Already have an subscription");
+    }
+    const options = {
+        amount: Number(119 * 100),
+        currency: "INR",
+        automatic_payment_methods: { enabled: true },
+        metadata: {
+            user_id: userId?.toString(),
+        }
+    };
+    const paymentIntent = await stripe.paymentIntents.create(options);
+    res.status(201).json({
+        paymentIntent,
+        message: "Checkout Successfull"
+    });
+});
+export const paymentVerification = TryCatch(async (req, res) => {
+    const user = req.user;
+    if (!req.user) {
+        new ErrorHandler(401, "No valid User");
+    }
+    // {
+    //   "id": "pi_3NxABC123xyz",
+    //   "object": "payment_intent",
+    //   "amount": 11900,
+    //   "currency": "inr",
+    //   "status": "requires_payment_method",
+    //   "client_secret": "pi_3NxABC123xyz_secret_xxxxxxxxx",
+    //   "metadata": {
+    //     "user_id": "64f123abc456"
+    //   },
+    //   "created": 1709123456
+    // }
+});
---
+++ services/payment/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import Stripe from "stripe";
+import cors from "cors";
+import paymentRoutes from "./routes/payment.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/payment", paymentRoutes);
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+app.listen(process.env.PORT, ()=> {
+  console.log("Payment service is listening at port:", process.env.PORT);
+});
